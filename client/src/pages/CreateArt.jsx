52f56020f1948abf248f8cf364182a16bf791b5a
William Mendez <emeryco@gmail.com>
2024-01-25 09:40:48 -0500

    updating the front end and adding neede functionality to the components

--- client/src/pages/CreateArt.jsx
+++ client/src/pages/CreateArt.jsx
@@ -1,10 +1,63 @@
-import React from 'react'
-
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { preview } from "../assets";
+import { getRandomPrompt } from "../utils";
+import { FormInput, Loading } from "../components";
 
 const CreateArt = () => {
-  return (
-    <div>CreateArt</div>
-  )
-}
+	const navigate = useNavigate();
+	const [form, setForm] = useState({
+		name: "",
+		prompt: "",
+		photo: "",
+	});
 
-export default CreateArt
+	const [generatingImg, setGeneratingImg] = useState(false);
+	const [loading, setLoading] = useState(false);
+
+  const handleSubmit = () => {
+
+  }
+
+  const handleChange = (e) => {
+
+
+  }
+
+	return (
+		<section className="max-w-7xl mx-auto">
+			<div>
+				<h1 className="font-extrabold text-[#222328] text-[25px]">
+					Create
+				</h1>
+				<p className="mt-2 text-[#666e75] text-[12px] max-w[500]">
+					{" "}
+					Put to the try your skills specifically indicating DALL-E the kind of Drawing you woul like to get done{" "}
+				</p>
+			</div>
+
+      <form className="mt-16 max-w-3xl" onSubmit={handleSubmit}>
+        <div className="flex flex-col gap-5">
+          <FormInput
+            label="Your name"
+            type="text"
+            name="name"
+            placehilder="Fulanito de tal"
+            handleChange={handleChange}
+          />
+
+          <FormInput
+            label="Prompt"
+            type="text"
+            name="prompt"
+            placehilder="Fulanito de tal"
+            handleChange={handleChange}
+          />
+
+        </div>
+      </form>
+		</section>
+	);
+};
+
+export default CreateArt;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,64 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { FormInput, Card, Loading } from "../components";
+
+const RenderCards = ({ data, title }) => {
+	if (data?.length > 0) {
+		return data.map((post) => <Card key={post._id} {...post} />);
+	}
+
+	return (
+		<h2 className="mt-5 font-bold text-[#6449ff] text-xl uppercase">{title}</h2>
+	);
+};
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+	const [loading, setLoading] = useState(false);
+	const [allPost, setPost] = useState(null);
+	const [searchText, useSearchText] = useState("");
 
-export default Home
+	return (
+		<section className="max-w-7xl mx-auto">
+			<div>
+				<h1 className="font-extrabold text-[#222328] text-[25px]">
+					The comunity showcase
+				</h1>
+				<p className="mt-2 text-[#666e75] text-[12px] max-w[500]">
+					{" "}
+					The best alternative to make use of the amaxing AI experience by
+					leveraging the power of DALL-E{" "}
+				</p>
+			</div>
+
+			<div className="mt-16">
+				<FormInput />
+			</div>
+      
+			<div className="mt-10">
+				{loading ? (
+					<div className="flex justify-center items center">
+						<Loading />
+					</div>
+				) : (
+					<>
+						{searchText && (
+							<h2 className="font-medium text-[#666e75] text-xl mb-3">
+								Sowing results for{" "}
+								<span classname="text-[#222328]">{searchText}</span>
+							</h2>
+						)}
+
+						<div className="grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-clos-1 gap-3 ">
+							{searchText ? (
+								<RenderCards data={[]} title="Search elements not found" />
+							) : (
+								<RenderCards data={[]} title="No posts found" />
+							)}
+						</div>
+					</>
+				)}
+			</div>
+		</section>
+	);
+};
+
+export default Home;
