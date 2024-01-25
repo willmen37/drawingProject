8462e19f99766299e2d3db55c59782e2538b4034
William Mendez <emeryco@gmail.com>
2024-01-25 15:47:35 -0500

    middleware connection

--- client/src/pages/CreateArt.jsx
+++ client/src/pages/CreateArt.jsx
@@ -15,7 +15,29 @@ const CreateArt = () => {
 	const [generatingImg, setGeneratingImg] = useState(false);
 	const [loading, setLoading] = useState(false);
 
-  const generateImage = () => {
+  const generateImage = async () => {
+    if(form.prompt){
+      try{
+        setGeneratingImg(true);
+        const response = await fetch("http://localhost:5000/api/v1/openaiRoute",{
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({prompt: form.prompt})
+        })
+
+        const data = await response.json();
+
+        setForm({...form, photo: `data:image/jpeg;base64,${data.photo}`})
+      }catch(error){
+        alert(error)
+      }finally{
+        setGeneratingImg(false)
+      }
+    }else{
+      alert("Please type what you want to draw")
+    }
 
   }
 
