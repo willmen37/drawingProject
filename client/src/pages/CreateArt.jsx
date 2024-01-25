da565fd7c6f95d4d120b901dbe876a18b5126ac1
William Mendez <emeryco@gmail.com>
2024-01-25 11:14:47 -0500

    creating basic functionality for the front end components such as vews an logic

--- client/src/pages/CreateArt.jsx
+++ client/src/pages/CreateArt.jsx
@@ -15,14 +15,27 @@ const CreateArt = () => {
 	const [generatingImg, setGeneratingImg] = useState(false);
 	const [loading, setLoading] = useState(false);
 
+  const generateImage = () => {
+
+  }
+
   const handleSubmit = () => {
 
   }
 
   const handleChange = (e) => {
+    setForm({...form, [e.target.name]: e.target.value})
 
 
   }
+
+  const handleSurpriseMe = () =>{
+    const randomPrompt = getRandomPrompt(form.prompt)
+    setForm({...form, prompt: randomPrompt})
+
+  }
+
+
 
 	return (
 		<section className="max-w-7xl mx-auto">
@@ -39,21 +52,64 @@ const CreateArt = () => {
       <form className="mt-16 max-w-3xl" onSubmit={handleSubmit}>
         <div className="flex flex-col gap-5">
           <FormInput
-            label="Your name"
+            labelName="Your name"
             type="text"
             name="name"
-            placehilder="Fulanito de tal"
+            placeholder="Fulanito de tal"
+            value={form.name}
             handleChange={handleChange}
           />
 
           <FormInput
-            label="Prompt"
+            labelName="Prompt"
             type="text"
             name="prompt"
-            placehilder="Fulanito de tal"
+            placeholder="'A realistic photograph of a young woman with blue eyes and blonde hair',"
+            value={form.prompt}
             handleChange={handleChange}
+            isSurpriseMe
+            handleSurpriseMe={handleSurpriseMe}
           />
 
+          <div className="relative bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-64 p-3 h-64 flex-justify-center items-center">
+            {form.photo? (
+              <img
+                src={form.photo}
+                alt={form.prompt}
+                className="w-full h-full object-contain"
+              />
+            ):(
+              <img
+                src={preview}
+                alt="preview"
+                className="w-9/12 h-9/12 object-contain opacity-40"
+              />
+            )}
+
+            {generatingImg && (
+              <div className="absolute inset-0 z-0 flex justify-center items-center bg-[rgba(0,0,0,0.5)] rounded-lg">
+                <Loading/>
+              </div>
+            ) }
+          </div>
+        </div>
+        <div className="mt-5 flex gap-5">
+              <button
+                type="button"
+                onClick={generateImage}
+                className="text-white bg-blue-700 font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5"
+              >
+              {generatingImg? "Creating art..." : "Create"}
+              </button>    
+        </div>
+        <div className="mt-10">
+              <p className="mt-2 text-[#666e75] text-[16px]">Share your art with the drAwIng comunity</p>
+              <button
+                type="submit"
+                className="mt-2 text-white bg-blue-700 font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5"
+              >
+               {loading? "uplaoding": "Upload Art Piece"}
+              </button>    
         </div>
       </form>
 		</section>
