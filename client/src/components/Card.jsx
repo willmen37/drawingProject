bee2a974654724dc94955f953b9135a95c4651a8
William Mendez <emeryco@gmail.com>
2024-01-31 09:42:29 -0500

    card styling propperties modified

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,24 +1,22 @@
 import React from 'react'
 
-import {download} from "../assets"
-import {downloadImage} from "../utils"
-
-const Card = ( {_id, name, prompt, photo}) => {
+const Card = ( {name, prompt, photo}) => {
   return (
+    
     <div className='rounded-xl group relative shadow-card hover:shadow-cardhover card'>
       <img
         className='w-full h-auto object-cover rounded-xl'
         src={photo}
         alt={prompt}
       />
-      <div className='group-hover:flex flex-col max-h-[94.5%] hidden absolute bottom-0 left-0 right-0 bg-[#10131f] m-2 p-4 rounded-md'>
-        <p className='text-white text-md overflow-y-auto prompt'>{prompt}</p>
+      <div className='group-hover:flex flex-col max-h-[94.5%] hidden absolute bottom-0 left-0 right-0 bg-white bg-opacity-50 m-2 p-4 rounded-md'>
+        <p className='font bold text-black text-[20px] overflow-y-auto prompt'>{prompt}</p>
 
         <div className='mt-5 flex justify-between items-center gap-2'>
-          <div className='w-7 h-7 rounded-full object-cover bg-green-700 flex justify-center items-center text-white text-xs font-bold'>
-            {name[0]}
+          <div className='w-7 h-7 rounded-full object-cover bg-red-700 flex justify-center items-center text-white text-xs font-bold'>
+            {name[0].toUpperCase()}
           </div>
-          <p className='text-white text-sm'>{name}</p>
+          <p className='font-extrabold text-red-700  text-sm'>{name}</p>
         </div>
       </div>
       
