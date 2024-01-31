2dd3402e2e5938b68fc6e1850f46ba96ca4f6a29
William Mendez <emeryco@gmail.com>
2024-01-31 09:40:24 -0500

    index. js properties added

--- client/src/pages/index.jsx
+++ client/src/pages/index.jsx
@@ -1,9 +1,17 @@
 import Home from "./Home";
 import CreateArt from "./CreateArt";
-import About from "./About"
+import About from "./About";
+import Login from "./Login";
+import Register from "./Register"
+import Profile from "./Profile"
+import TryMe from "./TryMe"
 
 export{
     Home,
     CreateArt,
-    About
+    About,
+    Login,
+    Register,
+    Profile,
+    TryMe
 }
