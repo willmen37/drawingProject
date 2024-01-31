564841604f70a00b5681e8667bb30b9222d78389
William Mendez <emeryco@gmail.com>
2024-01-31 09:47:11 -0500

    server.js has been updated according to perfomed changes on the backend files

--- backend/server.js
+++ backend/server.js
@@ -2,19 +2,32 @@ import express from "express"
 import * as dotenv from "dotenv"
 import cors from "cors"
 
+
 import connectDB from "./mongodb/connect.js";
 import postRoutes from "./routes/postRoutes.js"
 import openaiRoute from "./routes/openaiRoute.js"
+import authRoutes from "./routes/authRoutes.js"
+import userRoutes from "./routes/userRoutes.js"
+import authorize from "./middleware/authMiddleware.js"
 
 const PORT = 5000
+
+
+
 
 dotenv.config();
 const app = express();
 app.use(cors());
-                   //what is this??
-app.use(express.json( {limit:"50mb"}))
+                   
+app.use(express.json({ limit: '50mb' }))
 app.use("/api/v1/posts", postRoutes)
 app.use("/api/v1/openaiRoute", openaiRoute)
+
+//NEW CODE
+app.use("/auth", authRoutes)
+app.use("/api/users", authorize, userRoutes)
+
+
 
 app.get("/", async (req, res)=> {
     res.send("HOLA from the SERVER")
