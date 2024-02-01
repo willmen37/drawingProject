ae9135d9a31d5e3a6d98f1ee190afafa9f2c77bb
William Mendez <emeryco@gmail.com>
2024-02-01 09:50:34 -0500

    render changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,11 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      "/auth": "http://localhost:5000",
-      "/api": "http://localhost:5000"
-    }
-  },
+  // server: {
+  //   proxy: {
+  //     "/auth": "http://localhost:5000",
+  //     "/api": "http://localhost:5000"
+  //   }
+  // },
   plugins: [react()],
 })
