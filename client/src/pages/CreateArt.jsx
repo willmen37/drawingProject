56e62ced031240ceb190cfc2ee897e6fe9608755
William Mendez <emeryco@gmail.com>
2024-02-01 17:11:25 -0500

    changes

--- client/src/pages/CreateArt.jsx
+++ client/src/pages/CreateArt.jsx
@@ -4,7 +4,6 @@ import { preview } from "../assets";
 import { getRandomPrompt } from "../utils";
 import { FormInput, Loading } from "../components";
 
-
 const CreateArt = () => {
 	const navigate = useNavigate();
 	const [form, setForm] = useState({
@@ -16,154 +15,156 @@ const CreateArt = () => {
 	const [generatingImg, setGeneratingImg] = useState(false);
 	const [loading, setLoading] = useState(false);
 
-  const generateImage = async () => {
-    if(form.prompt){
-      try{
-        console.log("saving img on cloud")
-        setGeneratingImg(true);
-        const response = await fetch("https://drawingfront.onrender.com/api/v1/openaiRoute",{
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({prompt: form.prompt})
-        })
+	const generateImage = async () => {
+		if (form.prompt) {
+			try {
+				console.log("saving img on cloud");
+				setGeneratingImg(true);
+				const response = await fetch(
+					"https://drawingproject.onrender.com/api/v1/openaiRoute",
+					{
+						method: "POST",
+						headers: {
+							"Content-Type": "application/json",
+						},
+						body: JSON.stringify({ prompt: form.prompt }),
+					}
+				);
 
-        const data = await response.json();
+				const data = await response.json();
 
-        setForm({...form, photo: `data:image/jpeg;base64,${data.photo}`})
-      }catch(error){
-        alert(error)
-      }finally{
-        setGeneratingImg(false)
-      }
-    }else{
-      alert("Please type what you want to draw")
-    }
+				setForm({ ...form, photo: `data:image/jpeg;base64,${data.photo}` });
+			} catch (error) {
+				alert(error);
+			} finally {
+				setGeneratingImg(false);
+			}
+		} else {
+			alert("Please type what you want to draw");
+		}
+	};
 
-  }
+	const handleSubmit = async (e) => {
+		e.preventDefault();
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+		if (form.prompt && form.photo) {
+			setLoading(true);
+			console.log(form);
+			try {
+				const response = await fetch(
+					"https://drawingproject.onrender.com/api/v1/posts",
+					{
+						method: "POST",
+						headers: {
+							"Content-Type": "application/json",
+						},
+						body: JSON.stringify(form),
+					}
+				);
 
-    if(form.prompt && form.photo) {
-      setLoading(true);
-      console.log(form)
-      try{
-        const response = await fetch("https://drawingfront.onrender.com/api/v1/posts", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(form)
-        })
+				await response.json();
+				navigate("/");
+			} catch (err) {
+				alert(err);
+			}
+			setLoading(false);
+		} else {
+			alert("Type something to get an image");
+		}
+	};
 
-        await response.json();
-        navigate("/")
-      }catch(err){
-        alert(err)
+	const handleChange = (e) => {
+		setForm({ ...form, [e.target.name]: e.target.value });
+	};
 
-      }
-      setLoading(false)
-    }else{
-      alert("Type something to get an image")
-    }
-
-  }
-
-  const handleChange = (e) => {
-    setForm({...form, [e.target.name]: e.target.value})
-
-
-  }
-
-  const handleSurpriseMe = () =>{
-    const randomPrompt = getRandomPrompt(form.prompt)
-    setForm({...form, prompt: randomPrompt})
-
-  }
-
-
+	const handleSurpriseMe = () => {
+		const randomPrompt = getRandomPrompt(form.prompt);
+		setForm({ ...form, prompt: randomPrompt });
+	};
 
 	return (
 		<section className="max-w-7xl mx-auto">
 			<div>
 				<h1 className="font-extrabold text-[#EF4444] text-[40px]">
-					Welcome to  the drAwIng Art Shop!
+					Welcome to the drAwIng Art Shop!
 				</h1>
 				<p className="mt-2 text-[#A5B4FC] text-[25px] max-w[500]">
 					Be creative and specific while typing your artisitic ideas into DALL-E
 				</p>
 			</div>
 
-      <form className="mt-16 max-w-3xl" onSubmit={handleSubmit}>
-        <div className="flex flex-col gap-5 text-[#4F46E5]">
-        
+			<form className="mt-16 max-w-3xl" onSubmit={handleSubmit}>
+				<div className="flex flex-col gap-5 text-[#4F46E5]">
+					<FormInput
+						labelName={
+							<p className="text-[#A5B4FC] text-[38px]">Dall-E Input</p>
+						}
+						type="text"
+						name="prompt"
+						placeholder="Press the Try me! button or type your next art hit!"
+						value={form.prompt}
+						handleChange={handleChange}
+						isSurpriseMe
+						handleSurpriseMe={handleSurpriseMe}
+					/>
 
-          <FormInput
-            labelName={<p className="text-[#A5B4FC] text-[38px]">Dall-E Input</p>}
-            type="text"
-            name="prompt"
-            placeholder="Press the Try me! button or type your next art hit!"
-            value={form.prompt}
-            handleChange={handleChange}
-            isSurpriseMe
-            handleSurpriseMe={handleSurpriseMe}
-          />
+					<div className="relative bg-gray-50 border border-gray-300 text-gray text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-70 p-3 h-70 flex-justify-center items-center">
+						{form.photo ? (
+							<img
+								src={form.photo}
+								alt={form.prompt}
+								className="w-full h-full object-contain"
+							/>
+						) : (
+							<img
+								src={preview}
+								alt="preview"
+								className="w-9/12 h-9/12 object-contain opacity-50"
+							/>
+						)}
 
-          <div className="relative bg-gray-50 border border-gray-300 text-gray text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-70 p-3 h-70 flex-justify-center items-center">
-            {form.photo? (
-              <img
-                src={form.photo}
-                alt={form.prompt}
-                className="w-full h-full object-contain"
-              />
-            ):(
-              <img
-                src={preview}
-                alt="preview"
-                className="w-9/12 h-9/12 object-contain opacity-50"
-              />
-            )}
+						{generatingImg && (
+							<div className="absolute inset-0 z-0 flex justify-center items-center bg-[rgba(0,0,0,0.5)] rounded-lg">
+								<Loading />
+							</div>
+						)}
+					</div>
+				</div>
+				<br />
 
-            {generatingImg && (
-              <div className="absolute inset-0 z-0 flex justify-center items-center bg-[rgba(0,0,0,0.5)] rounded-lg">
-                <Loading/>
-              </div>
-            ) }
-          </div>
-        </div>
-        <br/>
+				<FormInput
+					labelName={
+						<p className="text-[#A5B4FC] text-[25px] mb-2">
+							Sign your Artwork:
+						</p>
+					}
+					type="text"
+					name="name"
+					placeholder="Artist Name"
+					value={form.name}
+					handleChange={handleChange}
+				/>
+				<div className="flex justify-between ">
+					<div className="mt-5 flex gap-5">
+						<button
+							type="button"
+							onClick={generateImage}
+							className=" mb-1 bg-[#EF4444] py-1 px-2 rounded text-white border-solid border-2 text-[22px]"
+						>
+							{generatingImg ? "Creating art..." : "Create"}
+						</button>
+					</div>
 
-        <FormInput
-        labelName={<p className="text-[#A5B4FC] text-[25px] mb-2">Sign your Artwork:</p>}
-        type="text"
-        name="name"
-        placeholder="Artist Name"
-        value={form.name}
-        handleChange={handleChange}
-        />
-        <div className="flex justify-between ">      
-          <div className="mt-5 flex gap-5">
-                <button
-                  type="button"
-                  onClick={generateImage}
-                  className=' mb-1 bg-[#EF4444] py-1 px-2 rounded text-white border-solid border-2 text-[22px]'
-                >
-                {generatingImg? "Creating art..." : "Create"}
-                </button>    
-          </div>
-          
-          <div className="mt-5">
-                <button
-                  type="submit"
-                  className= 'mb-1 bg-[#EF4444] py-1 px-2 rounded text-white border-solid border-2 text-[22px]'
-                >
-                {loading? "Uploading...": "Upload Art Piece into our Gallery"}
-                </button>    
-          </div>
-        </div>
-      </form>
+					<div className="mt-5">
+						<button
+							type="submit"
+							className="mb-1 bg-[#EF4444] py-1 px-2 rounded text-white border-solid border-2 text-[22px]"
+						>
+							{loading ? "Uploading..." : "Upload Art Piece into our Gallery"}
+						</button>
+					</div>
+				</div>
+			</form>
 		</section>
 	);
 };
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,108 +1,116 @@
-import axios from 'axios'
+import axios from "axios";
 
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faUnlockKeyhole } from '@fortawesome/free-solid-svg-icons'
-import baseURL from '../Api';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUnlockKeyhole } from "@fortawesome/free-solid-svg-icons";
+import baseURL from "../Api";
 
-
-let emptyForm = { 
-    username: '',
-    password: ''
-}
+let emptyForm = {
+	username: "",
+	password: "",
+};
 
 function Login({ setUser }) {
+	const navigate = useNavigate();
 
-    const navigate = useNavigate()
+	let [form, setForm] = useState(emptyForm);
 
-    let [form, setForm] = useState(emptyForm)
+	const handleChange = (e) => {
+		setForm({ ...form, [e.target.name]: e.target.value });
+	};
 
-    const handleChange = (e) => {
-        setForm({ ...form, [e.target.name]: e.target.value })
-    }
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		try {
+			const response = await axios.post(
+				"https://drawingproject.onrender.com/auth/login",
+				form
+			);
+			const token = response.data;
 
-    const handleSubmit = async (e) => {
-        e.preventDefault()
-        try {
+			console.log(token);
 
-            const response = await axios.post('https://drawingfront.onrender.com/auth/login', form)
-            const token = response.data
+			if (!token) {
+				setForm(emptyForm);
+				return;
+			}
 
-            console.log(token)
+			localStorage.setItem("token", token);
 
-            if (!token) {
-                setForm(emptyForm)
-                return
-            } 
+			const userResponse = await axios.get(
+				"https://drawingproject.onrender.com/api/users",
+				{
+					headers: {
+						Authorization: token,
+					},
+				}
+			);
 
-            localStorage.setItem("token", token)
+			setUser(userResponse.data);
 
-            const userResponse = await axios.get('https://drawingfront.onrender.com/api/users', { 
-                headers: {
-                    Authorization: token
-                }
-            })
+			navigate("/create-art");
+		} catch (err) {
+			console.log(err.response.data.error);
+			alert(err.response.data.error);
+		}
+	};
 
-            setUser(userResponse.data)
-    
-            navigate('/create-art')
+	return (
+		<>
+			<section className="mt-15 max-w-4xl mx-auto p-12 bg-white bg-opacity-15 rounded-md p-9">
+				<div className="mt-12 mb-9 ml-20 mr-20">
+					<div className="flex flex-col  items-center  my-12">
+						<FontAwesomeIcon
+							className="mt-5 text-[60px] w-20"
+							icon={faUnlockKeyhole}
+							beatFade
+							style={{ color: "#EF4444" }}
+						/>
 
-        } catch(err) {
-            console.log(err.response.data.error)
-            alert(err.response.data.error)
-        }
-    }
+						<p className="mt-16 text-white text-[30px] text-center max-w[500]">
+							Unlock you imagination with drAwIng, login now and enjoy the best
+							experience on handling Dall-E API
+						</p>
+					</div>
+					<form onSubmit={handleSubmit} className="mt-16 max-w-3xl">
+						<div className="flex flex-col  gap-4 ">
+							<p className="text-white text-center text-[25px]">Username:</p>
 
-    return ( 
-        <>
-          <section className="mt-15 max-w-4xl mx-auto p-12 bg-white bg-opacity-15 rounded-md p-9">
-            <div className='mt-12 mb-9 ml-20 mr-20'>
-            <div className="flex flex-col  items-center  my-12">
-                <FontAwesomeIcon
-                className="mt-5 text-[60px] w-20"
-                icon={faUnlockKeyhole}
-                beatFade
-                style={{ color: "#EF4444" }}
-                />
+							<input
+								type="text"
+								id="username"
+								name="username"
+								onChange={handleChange}
+								value={form.username}
+								className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#4649ff] focus:border-[#4649ff] outline-none block w-full p-3"
+							/>
+							<br />
+							<br />
+							<br />
+							<p className="text-white text-center text-[25px]">Password:</p>
 
-                <p className="mt-16 text-white text-[30px] text-center max-w[500]">
-					Unlock you imagination with drAwIng, login now and enjoy the best experience on handling Dall-E API
-				</p>
-            
-            </div>
-            <form onSubmit={handleSubmit} className="mt-16 max-w-3xl">
-            <div className="flex flex-col  gap-4 ">
-                <p className="text-white text-center text-[25px]">Username:</p>
-               
-                <input 
-                    type="text" 
-                    id="username"
-                    name="username"
-                    onChange={handleChange}
-                    value={form.username}
-                    className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#4649ff] focus:border-[#4649ff] outline-none block w-full p-3'
-                />
-                <br /><br /><br/>
-                <p className="text-white text-center text-[25px]" >Password:</p>
-                
-                <input 
-                    type="password" 
-                    id="password"
-                    name="password"
-                    onChange={handleChange}
-                    value={form.password}
-                    className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#4649ff] focus:border-[#4649ff] outline-none block w-full p-3'
-                />
-                <br /><br /><br/>
-                <button className="  mt-10 mb-1 bg-[#EF4444] py-1 px-2 rounded text-white border-solid border-2 text-[22px]" >Submit</button>
-            </div>
-            </form>
-          </div>
-          <br/>
-        </section>  
-        </>
-     );
+							<input
+								type="password"
+								id="password"
+								name="password"
+								onChange={handleChange}
+								value={form.password}
+								className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#4649ff] focus:border-[#4649ff] outline-none block w-full p-3"
+							/>
+							<br />
+							<br />
+							<br />
+							<button className="  mt-10 mb-1 bg-[#EF4444] py-1 px-2 rounded text-white border-solid border-2 text-[22px]">
+								Submit
+							</button>
+						</div>
+					</form>
+				</div>
+				<br />
+			</section>
+		</>
+	);
 }
 
 export default Login;
